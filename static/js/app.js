e558a737d8b151409f8e50fc8f92995414f50b7b
gfsiciliani <gfsiciliani@gmail.com>
2024-01-14 13:08:24 -0600

    configured data endpoint

---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+console.log('___GOOD MORNING, BELLY BUTTON OWNERS!!!___')
+
+// Get samples endpoint
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+// Fetch the JSON data and console log it
+d3.json(url).then(function(data) {
+    console.log(data);
+  });  
