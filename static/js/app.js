95bfd744ac70ab70a7cc35e5b34573bf5af10a37
gfsiciliani <gfsiciliani@gmail.com>
2024-01-22 14:59:27 -0600

    began js exploratory code

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-console.log('___GOOD MORNING, BELLY BUTTON OWNERS!!!___')
+console.log('___GOOD MORNING, BELLY BUTTON OWNERS!!!___');
 
 // Get samples endpoint
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
@@ -6,4 +6,16 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 // Fetch the JSON data and console log it
 d3.json(url).then(function(data) {
     console.log("JSON FETCHED", data);
+    
+    // get sample data
+    let sampleSet = data.samples[0] // adjust to be chosen by dropdown (see 14.3.10 for tutorial)
+    console.log("Sample set :", sampleSet);
+
+    // map data?
+    let otuIds = sampleSet.otu_ids;
+        // let otuIds = sampleSet.map(x => x.otu_ids);
+
+    console.log(otuIds)
+    let x = otuIds.map(x => x)
+    console.log(x)
 });
