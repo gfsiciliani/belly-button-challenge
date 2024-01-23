a7046c33100a720b2917c65183f1d06bb9ee8d6a
gfsiciliani <gfsiciliani@gmail.com>
2024-01-22 22:21:28 -0600

    visual progress

--- static/js/app.js
+++ static/js/app.js
@@ -6,16 +6,65 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 // Fetch the JSON data and console log it
 d3.json(url).then(function(data) {
     console.log("JSON FETCHED", data);
-    
-    // get sample data
-    let sampleSet = data.samples[0] // adjust to be chosen by dropdown (see 14.3.10 for tutorial)
-    console.log("Sample set :", sampleSet);
-
-    // map data?
-    let otuIds = sampleSet.otu_ids;
-        // let otuIds = sampleSet.map(x => x.otu_ids);
-
-    console.log(otuIds)
-    let x = otuIds.map(x => x)
-    console.log(x)
 });
+
+
+function buildBarChart(sampleID) {
+
+    d3.json(url).then(data => {
+        
+        // Get all sample data
+        let sampleData = data.samples;
+        
+        // isolate given ID's sample data
+        let sampleSelected = sampleData[sampleID];
+    
+        // parse sample data
+        let otu_ids = sampleSelected.otu_ids;
+        let otu_labels = sampleSelected.otu_labels;
+        let sample_values = sampleSelected.sample_values;
+
+        // log sample data
+        console.log("OTU IDs: ", otu_ids, "Labels: ", otu_labels, "Values: ", sample_values);
+
+        // get top ten
+        let xData = sample_values.slice(0,10).reverse();
+        let yData = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();  // WHY????
+        let labels = otu_labels.slice(0,10).reverse(); 
+        console.log("OTU IDs: ", yData, "Labels: ", xData, "Values: ", labels);
+
+
+        // define trace for bar chart
+        let trace = {
+            x: xData,
+            y: yData,
+            text: labels,
+            type: "bar",
+            orientation: "h",
+            marker: {
+                color: 'rgba(55, 128, 191, 0.7)',
+                    line: {
+                        color: 'rgba(55, 128, 191, 1.0)',
+                        width: 1.5
+                    }
+            }
+        };
+
+        let chartData = [trace]
+
+        // define layout
+        let layout = {
+            title: `Top 10 OTUs present in ID: ${sampleID}`,
+            bargap: 0.40,
+            paper_bgcolor: 'rgba(245, 246, 249, 1)',
+            plot_bgcolor: 'rgba(245, 246, 249, 0)'
+        };
+
+        // plot the bar chart
+        Plotly.newPlot("bar", chartData, layout);
+    });
+
+
+};
+
+buildBarChart(0);
