77e9d2203da8f366e2e4b346330aafddbf3784cf
gfsiciliani <gfsiciliani@gmail.com>
2024-01-23 11:55:43 -0600

    began bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,16 @@
-console.log('___GOOD MORNING, BELLY BUTTON OWNERS!!!___');
+// console.log('___GOOD MORNING, BELLY BUTTON OWNERS!!!___');
 
 // Get samples endpoint
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 // Fetch the JSON data and console log it
+console.log("FETCHING JSON...")
 d3.json(url).then(function(data) {
-    console.log("JSON FETCHED", data);
+    console.log("***JSON FETCHED SUCCESSFULLY***");
+    console.log(data);
 });
 
-
+// [2] - building bar chart that shows the top 10 OTUs in the given individual in the dropdown
 function buildBarChart(sampleID) {
 
     d3.json(url).then(data => {
@@ -23,17 +25,15 @@ function buildBarChart(sampleID) {
         let otu_ids = sampleSelected.otu_ids;
         let otu_labels = sampleSelected.otu_labels;
         let sample_values = sampleSelected.sample_values;
-
-        // log sample data
-        console.log("OTU IDs: ", otu_ids, "Labels: ", otu_labels, "Values: ", sample_values);
-
+        
         // get top ten
         let xData = sample_values.slice(0,10).reverse();
         let yData = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();  // WHY????
         let labels = otu_labels.slice(0,10).reverse(); 
-        console.log("OTU IDs: ", yData, "Labels: ", xData, "Values: ", labels);
-
-
+        
+        // log sample data
+        // console.log("OTU IDs: ", yData, "Labels: ", xData, "Values: ", labels[0]);
+        
         // define trace for bar chart
         let trace = {
             x: xData,
@@ -50,6 +50,7 @@ function buildBarChart(sampleID) {
             }
         };
 
+        // put trace in an array for Plotly
         let chartData = [trace]
 
         // define layout
@@ -63,8 +64,16 @@ function buildBarChart(sampleID) {
         // plot the bar chart
         Plotly.newPlot("bar", chartData, layout);
     });
-
-
 };
 
-buildBarChart(0);
+buildBarChart(0); // adjust this to take the dropdown selection (14.3.last)
+
+// [3] - building bubble chart displaying each sample
+function buildBubbleChart(sampleID) {
+
+
+    // define data
+    let otu_ids = sampleSelected.otu_ids; // [X AND colors] JUST AN ID NUMBER, NOT A MEASUREMENT
+    let sample_values = sampleSelected.sample_values; // [Y AND marker size] this is a measurement
+    let otu_labels = sampleSelected.otu_labels; // [text]
+}
